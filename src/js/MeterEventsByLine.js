61144eaa7f6de16b40a6b08435840b19bd4aee42
Billy Ernest <bernest@gridprotectionalliance.org>
2016-03-29 11:04:51 -0400

    Added CSA button and function calls to MeterEventsByLine.js

--- src/js/MeterEventsByLine.js
+++ src/js/MeterEventsByLine.js
@@ -55,7 +55,8 @@ function populateMeterEventsDivWithGrid(thedatasource, thediv, siteName, siteID,
                         { name: 'thefaulttype' },
                         { name: 'thecurrentdistance' },
                         { name: 'pqiexists' },
-                        { name: 'easexists' }
+                        { name: 'easexists' },
+                        { name: 'csaexists'}
                     ]
 
                 },
@@ -77,7 +78,8 @@ function populateMeterEventsDivWithGrid(thedatasource, thediv, siteName, siteID,
                 { text: ' ', cellsrenderer: makeOpenSeeStackButton_html, dataField: 'OpenSeeStack', width: 40, padding: 0, cellsalign: 'left' },
                 { text: ' ', cellsrenderer: makeFaultSpecificsButton_html, dataField: 'FaultSpecifics', width: 40, padding: 0, cellsalign: 'left' },
                 { text: ' ', cellsrenderer: makePQIButton_html, dataField: 'PQI', width: 40, padding: 0, cellsalign: 'left' },
-                { text: ' ', cellsrenderer: makeEASDetailsButton_html, dataField: 'EAS', width: 40, padding: 0, cellsalign: 'left' }
+                { text: ' ', cellsrenderer: makeEASDetailsButton_html, dataField: 'EAS', width: 40, padding: 0, cellsalign: 'left' },
+                { text: ' ', cellsrenderer: makeCSADetailsButton_html, dataField: 'CSA', width: 40, padding: 0, cellsalign: 'left' }
                 ]
             });
 
@@ -112,11 +114,34 @@ function makeEASDetailsButton_html(id) {
     return (return_html);
 }
 
+function makeCSADetailsButton_html(id) {
+    var return_html = "";
+
+    var datarow = $('#MeterDetailsByDate').jqxGrid('getrowdata', id);
+
+    if (datarow.easexists == "1") {
+
+        return_html += '<div style="cursor: pointer; width: 100%; Height: 100%; text-align: center; margin: auto; border: 0 none;">';
+        return_html += '<button onClick="OpenWindowToCSA(' + id + ');" value="" style="cursor: pointer; text-align: center; margin: auto; border: 0 none;" title="Launch ICF Details Page">';
+        return_html += '<img src="images/csa.png" /></button></div>';
+    }
+    return (return_html);
+}
+
 function OpenWindowToEAS(id) {
 
     var datarow = $('#MeterDetailsByDate').jqxGrid('getrowdata', id);
 
     var popup = window.open("ICFDetails.aspx?eventid=" + datarow.theeventid, datarow.theeventid + "ICFDetails", "left=0,top=0,width=300,height=200,status=no,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no");
+
+    return false;
+}
+
+function OpenWindowToCSA(id) {
+
+    var datarow = $('#MeterDetailsByDate').jqxGrid('getrowdata', id);
+
+    var popup = window.open("CSADetails.aspx?eventid=" + datarow.theeventid, datarow.theeventid + "CSADetails", "left=0,top=0,width=300,height=200,status=no,resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no");
 
     return false;
 }
