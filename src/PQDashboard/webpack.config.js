59ce09af542ab5a9f8f2bca61ac373b1059c4628
StephenCWills <swills@gridprotectionalliance.org>
2019-02-04 16:27:09 -0500

    Fixed requestAnimationFrame references in openSEE by renaming the webpack config variable.

--- src/PQDashboard/webpack.config.js
+++ src/PQDashboard/webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
           jQuery: 'jquery',
           Map: 'core-js/es6/map',
           Set: 'core-js/es6/set',
-          requestAnimationFrame: 'raf/polyfill'
+          __requestAnimationFrame__: 'raf/polyfill'
         })
     ]
 };
