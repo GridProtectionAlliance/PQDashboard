a844d6c54620093a2f2986725fb7b967d034438e
Billy Ernest <bernest@gridprotectionalliance.org>
2020-02-03 08:05:30 -0500

    Fixed SE Browser logo.

--- src/PQDashboard/Scripts/TSX/PQDashboard/PQDashboard.tsx
+++ src/PQDashboard/Scripts/TSX/PQDashboard/PQDashboard.tsx
@@ -51,7 +51,7 @@ class PQDashboard extends React.Component<{}, PQDashboardState>{
             <Router>
                 <div style={{ position: 'absolute', width: '100%', height: '100%', overflow: 'hidden' }}>
                     <div style={{ width: 300, height: 'inherit', backgroundColor: '#eeeeee', position: 'relative', float: 'left' }}>
-                        <a href="https://www.gridprotectionalliance.org"><img style={{ width: 280, margin: 10 }} src={homePath + "Images/SE Dashboard with GPA 200 high.png"} /></a>
+                        <a href="https://www.gridprotectionalliance.org"><img style={{ width: 280, margin: 10 }} src={homePath + "Images/SE Browser - Spelled out - 111 high.png"} /></a>
                         <div style={{ width: '100%', height: '100%', marginTop: 30}}>
                             <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical" style={{ height: 'calc(100% - 240px)' }}>
                                 {/*<NavLink activeClassName='nav-link active' className="nav-link" exact={true} to={controllerViewPath + "/"}>Home</NavLink>*/}
