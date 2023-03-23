c64d1d69ade9edcb7eeaf41d63df9a5b9a87b981
StephenCWills <swills@gridprotectionalliance.org>
2023-03-23 10:36:07 -0400

    Use APIQuery for HIDS queries

--- src/PQDashboard/XDAClient.cs
+++ src/PQDashboard/XDAClient.cs
@@ -23,15 +23,14 @@
 
 using System;
 using System.IO;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using GSF.Data;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using openXDA.APIAuthentication;
 
 namespace PQDashboard
 {
@@ -57,8 +56,6 @@ namespace PQDashboard
             "    Text = {0} AND " +
             "    GroupID = (SELECT ID FROM ValueListGroup WHERE Name = 'System')";
 
-        private const string RequestVerificationHeader = "X-GSF-Verify";
-
         #endregion
 
         #region [ Constructors ]
@@ -68,20 +65,16 @@ namespace PQDashboard
             using (AdoDataConnection connection = connectionFactory())
             {
                 BaseURL = Query(connection, "XDAInstance");
-
-                string user = Query(connection, "XDAUser");
-                string password = Query(connection, "XDAPassword");
-                Credential = $"{user}:{password}";
+                XDAUser = Query(connection, "XDAUser");
+                XDAPassword = Query(connection, "XDAPassword");
             }
         }
 
         public XDAClient(AdoDataConnection connection)
         {
             BaseURL = Query(connection, "XDAInstance");
-
-            string user = Query(connection, "XDAUser");
-            string password = Query(connection, "XDAPassword");
-            Credential = $"{user}:{password}";
+            XDAUser = Query(connection, "XDAUser");
+            XDAPassword = Query(connection, "XDAPassword");
         }
 
         #endregion
@@ -89,7 +82,8 @@ namespace PQDashboard
         #region [ Properties ]
 
         public string BaseURL { get; }
-        private string Credential { get; }
+        private string XDAUser { get; }
+        private string XDAPassword { get; }
 
         #endregion
 
@@ -112,10 +106,25 @@ namespace PQDashboard
         public async Task QueryHIDSPointsAsync(object query, Func<HIDSPoint, CancellationToken, Task> processAsync, CancellationToken cancellationToken = default(CancellationToken))
         {
             string url = $"{BaseURL}/api/hids/QueryPoints";
-            string requestVerificationHeaderToken = await GenerateRequestVerificationHeaderTokenAsync(cancellationToken);
+            APIQuery apiQuery = new APIQuery(XDAUser, XDAPassword, BaseURL);
 
-            using (HttpRequestMessage request = ConfigureRequest(HttpMethod.Post, url, requestVerificationHeaderToken, query))
-            using (HttpResponseMessage response = await HttpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            Action<HttpRequestMessage> configure = request =>
+            {
+                request.Method = HttpMethod.Post;
+
+                string json = JObject
+                    .FromObject(query)
+                    .ToString(Formatting.None);
+
+                MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
+                contentType.CharSet = "utf-8";
+
+                StringContent content = new StringContent(json);
+                content.Headers.ContentType = contentType;
+                request.Content = content;
+            };
+
+            using (HttpResponseMessage response = await apiQuery.SendWebRequestAsync(configure, "/api/hids/QueryPoints", cancellationToken))
             using (Stream stream = await response.Content.ReadAsStreamAsync())
             using (TextReader reader = new StreamReader(stream))
             {
@@ -138,55 +147,9 @@ namespace PQDashboard
             }
         }
 
-        private async Task<string> GenerateRequestVerificationHeaderTokenAsync(CancellationToken cancellationToken = default(CancellationToken))
-        {
-            using (HttpRequestMessage request = ConfigureRequest(HttpMethod.Get, $"{BaseURL}/api/rvht"))
-            using (HttpResponseMessage response = await HttpClient.SendAsync(request, cancellationToken))
-            {
-                response.EnsureSuccessStatusCode();
-                return await response.Content.ReadAsStringAsync();
-            }
-        }
-
-        private HttpRequestMessage ConfigureRequest(HttpMethod method, string url)
-        {
-            Encoding utf8 = new UTF8Encoding(false);
-            byte[] bytes = utf8.GetBytes(Credential);
-            string authToken = Convert.ToBase64String(bytes);
-
-            HttpRequestMessage request = new HttpRequestMessage(method, url);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", authToken);
-            return request;
-        }
-
-        private HttpRequestMessage ConfigureRequest(HttpMethod method, string url, string requestVerificationHeaderToken, object contentObj)
-        {
-            if (method == HttpMethod.Get)
-                throw new ArgumentException("GET method does not require verification and does not contain a body", nameof(method));
-
-            string json = JObject
-                .FromObject(contentObj)
-                .ToString(Formatting.None);
-
-            MediaTypeHeaderValue contentType = new MediaTypeHeaderValue("application/json");
-            contentType.CharSet = "utf-8";
-
-            StringContent content = new StringContent(json);
-            content.Headers.ContentType = contentType;
-
-            HttpRequestMessage request = ConfigureRequest(method, url);
-            request.Headers.Add(RequestVerificationHeader, requestVerificationHeaderToken);
-            request.Content = content;
-            return request;
-        }
-
         #endregion
 
         #region [ Static ]
-
-        // Static Properties
-        private static HttpClient HttpClient { get; }
-            = new HttpClient();
 
         // Static Methods
         private static string Query(AdoDataConnection connection, string field) =>
