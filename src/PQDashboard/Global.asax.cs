18d6a8b5b666f3dfec152bbf7e32eb94d61e4b67
Christoph Lackner <52460212+clackner-gpa@users.noreply.github.com>
2023-04-12 11:57:58 -0400

    Removed Role and GroupValidation in case they are in OpenXDA DB

--- src/PQDashboard/Global.asax.cs
+++ src/PQDashboard/Global.asax.cs
@@ -107,10 +107,10 @@ namespace PQDashboard
                 //EncryptScores(dataContext);
 
                 // Validate default security roles exist
-                ValidateSecurityRoles(connection, systemSettings["DefaultSecurityRoles"].Value);
+                //ValidateSecurityRoles(connection, systemSettings["DefaultSecurityRoles"].Value);
 
                 // Validate users and groups exist in the database as SIDs
-                ValidateAccountsAndGroups(connection);
+                //ValidateAccountsAndGroups(connection);
 
 
                 //// Load global web settings
@@ -194,115 +194,5 @@ namespace PQDashboard
 #endif
             }, DataHub.CurrentConnectionID);
         }
-
-        /// <summary>
-        /// Validates security roles for all defined nodes.
-        /// </summary>
-        /// <param name="database">Data connection to use for database operations.</param>
-        /// <param name="defaultSecurityRoles">Default security roles that should exist.</param>        
-        private static void ValidateSecurityRoles(AdoDataConnection database, string defaultSecurityRoles)
-        {
-            // Queries
-            const string RoleCountFormat = "SELECT COUNT(*) FROM ApplicationRole WHERE NodeID = {0} AND Name = {1}";
-
-            if (string.IsNullOrEmpty(defaultSecurityRoles))
-                defaultSecurityRoles = "Administrator, Owner, Viewer";
-
-            string[] roles = defaultSecurityRoles.Split(',').Select(role => role.Trim()).Where(role => !string.IsNullOrEmpty(role)).ToArray();
-
-            // For each Node in new database make sure all roles exist
-            DataTable dataTable = database.RetrieveData("SELECT ID FROM Node");
-
-            foreach (DataRow row in dataTable.Rows)
-            {
-                Guid nodeID = row.ConvertField<Guid>("ID");
-
-                foreach (string role in roles)
-                    if ((database.ExecuteScalar<int?>(RoleCountFormat, database.Guid(nodeID), role) ?? 0) == 0)
-                        AddRolesForNode(database, nodeID, role);
-            }
-        }
-
-        /// <summary>
-        /// Adds role for newly added node, e.g., Administrator, Editor, Viewer.
-        /// </summary>
-        /// <param name="database">Data connection to use for database operations.</param>
-        /// <param name="nodeID">Node ID to which roles are being assigned.</param>
-        /// <param name="roleName">Name of role to be added.</param>
-        private static void AddRolesForNode(AdoDataConnection database, Guid nodeID, string roleName)
-        {
-            // Queries
-            const string InsertRoleFormat = "INSERT INTO ApplicationRole(Name, Description, NodeID, UpdatedBy, CreatedBy) VALUES('{0}', '{0} Role', {{0}}, {{1}}, {{2}})";
-
-            string currentUserSID = UserInfo.UserNameToSID(UserInfo.CurrentUserID);
-            database.ExecuteNonQuery(string.Format(InsertRoleFormat, roleName), database.Guid(nodeID), currentUserSID, currentUserSID);
-        }
-
-        /// <summary>
-        /// Validate accounts and groups to ensure that account names and group names are converted to SIDs.
-        /// </summary>
-        /// <param name="database">Data connection to use for database operations.</param>
-        private static void ValidateAccountsAndGroups(AdoDataConnection database)
-        {
-            const string SelectUserAccountQuery = "SELECT ID, Name, UseADAuthentication FROM UserAccount";
-            const string SelectSecurityGroupQuery = "SELECT ID, Name FROM SecurityGroup";
-            const string UpdateUserAccountFormat = "UPDATE UserAccount SET Name = '{0}' WHERE ID = '{1}'";
-            const string UpdateSecurityGroupFormat = "UPDATE SecurityGroup SET Name = '{0}' WHERE ID = '{1}'";
-
-            string id;
-            string sid;
-            string accountName;
-            Dictionary<string, string> updateMap;
-
-            updateMap = new Dictionary<string, string>();
-
-            // Find user accounts that need to be updated
-            using (IDataReader userAccountReader = database.Connection.ExecuteReader(SelectUserAccountQuery))
-            {
-                while (userAccountReader.Read())
-                {
-                    id = userAccountReader["ID"].ToNonNullString();
-                    accountName = userAccountReader["Name"].ToNonNullString();
-
-                    if (userAccountReader["UseADAuthentication"].ToNonNullString().ParseBoolean())
-                    {
-                        sid = UserInfo.UserNameToSID(accountName);
-
-                        if (!ReferenceEquals(accountName, sid) && UserInfo.IsUserSID(sid))
-                            updateMap.Add(id, sid);
-                    }
-                }
-            }
-
-            // Update user accounts
-            foreach (KeyValuePair<string, string> pair in updateMap)
-                database.Connection.ExecuteNonQuery(string.Format(UpdateUserAccountFormat, pair.Value, pair.Key));
-
-            updateMap.Clear();
-
-            // Find security groups that need to be updated
-            using (IDataReader securityGroupReader = database.Connection.ExecuteReader(SelectSecurityGroupQuery))
-            {
-                while (securityGroupReader.Read())
-                {
-                    id = securityGroupReader["ID"].ToNonNullString();
-                    accountName = securityGroupReader["Name"].ToNonNullString();
-
-                    if (accountName.Contains('\\'))
-                    {
-                        sid = UserInfo.GroupNameToSID(accountName);
-
-                        if (!ReferenceEquals(accountName, sid) && UserInfo.IsGroupSID(sid))
-                            updateMap.Add(id, sid);
-                    }
-                }
-            }
-
-            // Update security groups
-            foreach (KeyValuePair<string, string> pair in updateMap)
-                database.Connection.ExecuteNonQuery(string.Format(UpdateSecurityGroupFormat, pair.Value, pair.Key));
-        }
-
-
     }
 }
